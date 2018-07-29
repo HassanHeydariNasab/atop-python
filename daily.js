788f79657c7a4c07dcb85eda8aea4a8d53bd214b
hsn6 <hsn6@openmailbox.org>
2018-07-29 10:28:27 +0430

    Add from last code base

---
+++ daily.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient
+const MONGODB_URL = 'mongodb://127.0.0.1:27017';
+const ObjectID = require('mongodb').ObjectID
+
+var db
+MongoClient.connect(MONGODB_URL, {
+    useNewUrlParser: true
+    }, (err, client) => {
+    if (err) return console.error('[DAILY] ERROR mongodb: '+err)
+    db = client.db('mutacio')
+    db.collection('users').updateMany({}, {$set: {remaining_posts: 100, remaining_likes: 100}}, (err, r) => {
+        if (err)
+            console.error('[DAILY] ERROR mongodb: '+err)
+        console.log('[DAILY] LOG mongodb: '+r)
+        client.close()
+        return;
+    })
+})
